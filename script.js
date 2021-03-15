e00214565bda3d5def97f1ca59f72ec35854bbd5
Alex Silva <alexsilva10san@gmail.com>
2021-03-15 18:13:05 -0300

    Fn

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const green = document.querySelector('.green');
 
+//criar ordem  aleátoria de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -18,6 +19,8 @@ let shuffleOrder = () => {
     }
 }
 
+
+//acende a proxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
@@ -27,3 +30,70 @@ let lightColor = (element, number) => {
         element.classList.remove('selected');
     });
 }
+
+// checa se os botões clicados são os mesmo da ordem gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+        if(clickedOrder.length ==  order.length){
+            alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+            nextLevel();
+        }
+    }
+}
+
+//função para o clique do usuário 
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+}
+
+//função que retorna a cor
+let createColorElement = (color) => {
+    if(color === 0) {
+        return green;
+    } else if(color === 1) {
+        return red;
+    }else if(color === 2) {
+        return yellow;
+    }else if(color === 3) {
+        return blue;
+    }
+}
+
+//fn proximo nivel de jogo
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//fn para game over
+let gameOver = () => {
+    alert(`Pontuação: ${score}\nVocê perdeu o jogo! Clique em OK para iniciar um novo jogo!`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () => {
+    alert(`Bem vindo ao Gênesis! Iniciando novo jogo!`);
+    score = 0;
+
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
