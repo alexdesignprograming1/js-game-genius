1a21cfdfe7b6af6a552d47d6e889ed5da40a72c3
Alex Silva <alexsilva10san@gmail.com>
2021-03-15 17:18:26 -0300

    create scripts

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 let order = [];
 let clickedOrder = [];
 let score = 0;
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number -250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
